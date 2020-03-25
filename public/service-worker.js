a8f2ecdfe5af970c4b8ee67f7b68ead332b4dd02
Glitch (diagnostic-elegant-scilla) <none>
2020-03-25 07:02:27 +0000

    👽🚥 Checkpoint ./public/index.html:3612174/244 ./public/service-worker.js:3612174/198

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,12 +22,18 @@ const CACHE_NAME = 'static-cache-v1';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
+  '/offline.html'
 ];
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
   // CODELAB: Precache static resources here.
-
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+);
   self.skipWaiting();
 });
 

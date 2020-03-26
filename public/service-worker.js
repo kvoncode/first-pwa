fb6c2fafb293e2a8c3758837107d98f7a72b1919
Glitch (diagnostic-elegant-scilla) <none>
2020-03-26 10:25:43 +0000

    🍜🚔 Checkpoint ./public/scripts/app.js:3612174/458 ./public/service-worker.js:3612174/725

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,11 +18,32 @@
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
-
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
-  '/offline.html'
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/scripts/install.js',
+  '/scripts/luxon-1.11.4.js',
+  '/styles/inline.css',
+  '/images/add.svg',
+  '/images/clear-day.svg',
+  '/images/clear-night.svg',
+  '/images/cloudy.svg',
+  '/images/fog.svg',
+  '/images/hail.svg',
+  '/images/install.svg',
+  '/images/partly-cloudy-day.svg',
+  '/images/partly-cloudy-night.svg',
+  '/images/rain.svg',
+  '/images/refresh.svg',
+  '/images/sleet.svg',
+  '/images/snow.svg',
+  '/images/thunderstorm.svg',
+  '/images/tornado.svg',
+  '/images/wind.svg',
 ];
 
 self.addEventListener('install', (evt) => {
@@ -43,7 +64,7 @@ self.addEventListener('activate', (evt) => {
   evt.waitUntil(
       caches.keys().then((keyList) => {
         return Promise.all(keyList.map((key) => {
-          if (key !== CACHE_NAME) {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log('[ServiceWorker] Removing old cache', key);
             return caches.delete(key);
           }

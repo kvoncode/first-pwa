e8c63361cdb3d1ddeda084c1dcc465980e4618a7
Glitch (diagnostic-elegant-scilla) <none>
2020-03-26 10:37:41 +0000

    ⏱🌁 Checkpoint ./public/service-worker.js:3612174/964

--- public/service-worker.js
+++ public/service-worker.js
@@ -77,10 +77,32 @@ self.addEventListener('activate', (evt) => {
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
   // CODELAB: Add fetch event handler here.
-  if (evt.request.mode !== 'navigate') {
-  // Not a page navigation, bail.
+  if (evt.request.url.includes('/forecast/')) {
+  console.log('[Service Worker] Fetch (data)', evt.request.url);
+  evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            }).catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+      }));
   return;
 }
+evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request)
+          .then((response) => {
+            return response || fetch(evt.request);
+          });
+    })
+);
 evt.respondWith(
     fetch(evt.request)
         .catch(() => {
